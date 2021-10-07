e7cbc7607d0efe1584b545d7b2e54c4e786a0980
hikeoneasy <71643984+hikeoneasy@users.noreply.github.com>
2021-10-07 23:34:25 +0900

    21.10.07
    아직 궁금한게 많은 나이

--- bignum.c
+++ bignum.c
@@ -12,12 +12,15 @@ bignum에 우리가 원하는 크기 정해주면(ex unsigned char, unsigned int
 #define NON_NEGATIVE 0
 #define NEGATIVE 1
 
+
 int Dtype = 0; 
 
 
 
 /*
     자료형의 크기를 선택할 수 있습니다.
+
+    이걸 Dtype 값을 0 1 -1 이런식으로 하고 싶은데 아이디어가 필요해요 위에처럼 global 변수로 선언하는게 맞을까요..?
 */
 if(Dtype = 0)
     typedef unsigned char word;
@@ -36,34 +39,44 @@ typedef struct {
 
 
 /*
-
 Zerorize 함수안에 arr init 구현하는건가요..???
+대충 느낌으로만 이렇게 구현함 ㅋㅋㅋ
+0 초기화 하는거 같아서,,,
+
+void array_init(word*a, int wordlen)
+{
+    for(int i=0;i<worlen;i++)
+    {
+        a[i] =  0x0;
+    }
+
+}
 */    
 
 
 //제거자
 void bignum_delete(bignum* x)
 {
-    if (* x == NULL )
+    if (*x == NULL )
         retrun ;
 #ifdef ZERORIZE                                 //얘 뭐임? ㅋㅋㅋㅋㅋㅋ
     array_init((*x)->a,(*x)->wordlen);          
 #endif
 
-    free ((* x ) ->a );
-    free (* x ) ;
+    free ((*x ) ->a );
+    free (*x ) ;
     *x = NULL ;
 }
 
 void bignum_new ( bignum ** x , int wordlen )
 {
-    if (* x != NULL )
+    if (*x != NULL )
     bignum_delete(x);
 
-    *x = ( bigint *) malloc ( sizeof ( bigint )) ;
+    *x = ( bignum *) malloc ( sizeof ( bignum )) ;
     (*x) -> sign = NON_NEGATIVE ; // 0
     (*x) -> wordlen = wordlen ;
-    (*x) ->a = ( word *) calloc ( wordlen , sizeof ( word ));
+    (*x) ->a = (word*) calloc ( wordlen , sizeof ( word ));
 }
 
 void bignum_refine ( bignum * x)
@@ -71,30 +84,30 @@ void bignum_refine ( bignum * x)
     if (x == NULL )
     return ;
 
-    int new_wordlen = x -> wordlen ;
+    int new_wordlen = x -> wordlen;
     while ( new_wordlen > 1) // at least one word needed
     {
         if (x -> a[ new_wordlen - 1] != 0)
             break ;
-        new_wordlen - -;
+        new_wordlen--;
     }
     if (x -> wordlen != new_wordlen )
     {
         x -> wordlen = new_wordlen ;
-        x ->a = ( word *) realloc (x - >a , sizeof ( word )* new_wordlen );
+        x ->a = ( word*) realloc (x - >a , sizeof( word )* new_wordlen );
     }
 
     if ((x - > wordlen == 1) && (x -> a [0] == 0x0 ))
         x -> sign = NON_NEGATIVE ;
 }
 
-void bignum_assign ( bignum ** y , bignum * x)
+void bignum_assign ( bignum ** y , bignum *x)
 {
-    if (* y != NULL )
+    if (*y != NULL )
     bignum_delete (y);
     bignum_new (y , x -> wordlen ); 
-    (* y) -> sign = x -> sign ;
-    array_copy ((* y ) ->a , x ->a , x -> wordlen );
+    (*y) -> sign = x -> sign ;
+    array_copy ((*y ) ->a , x ->a , x -> wordlen );
 }
 
 /*
@@ -108,40 +121,67 @@ void bi_show_bin(bigint* x); // show x in binary representation
 
 
 
-//u_int 형태의 data 넣기
-void bignum_setword(bignum *dest, word src)
-{
-    dest->dats[0]=src;
-    memset(&dest->dats[1], 0, (dest->len-1)*sizeof(word));
-}
-
-//bignum 형태의 데이터 넣기
-void bignum_set(bignum *dest, const bignum *src)
-{
-    memcpy(dest->dats,src->dats,
-        ((dest->len<src->len)?dest->len:src->len)*sizeof(word));
-}
-
 */  
+void bignum_set_by_array(bignum** x, int sign, word* a, int wordlen) // bigint x <- sign and array a
+{
+    if (*x != NULL )
+    bignum_delete (x);
+    bignum_new (x , wordlen );
+    (*x) -> sign = sign;
+    array_copy ((*x ) ->a , a , wordlen );
+
+}
+
+//맞게 구현한건가...?/???
+void binum_set_by_string(bigint** x, int sign, char* str, int base)// bigint x <- base string        //////////////////base가 무슨 의미일까,,, 스트링 길이???
+{
+if (*x != NULL )
+    bignum_delete (x);
+    bignum_new (x , wordlen );
+    (*x) -> sign = sign;
+    memcpy((*x ) ->a , str , base );
+}
+
+void bignum_show_hex(bigint* x)
+{
+    int len = (*x)->wordlen;
+    for(int i=0;i<len;i++)
+    {
+        printf("%x",(*x)->a[i]);
+    }
+    
+}
+
+void bignum_show_dec(bigint* x)
+{
+    // show얘네는 char int longlong나눠서 생각해야 하는거 아닌가요...?????
+    
+}
+
+void bignum_show_dec(bigint* x)
+{
+   
+    
+}
 
 
 void bignum_gen_rand ( bignum **x , int sign , int wordlen )
 {
     bignum_new (x , wordlen );
-    (* x) -> sign = sign ;
-    array_rand ((* x ) ->a , wordlen );
-    bignum_refine (* x);
+    (*x) -> sign = sign ;
+    array_rand ((*x) ->a , wordlen );
+    bignum_refine (*x);
 }
 
-void array_rand ( word * dst , int wordlen )
+void array_rand ( word* dst , int wordlen )
 {
-    byte * p = ( byte *) dst ;
+    byte*p = ( byte*) dst ;
     int cnt = wordlen * sizeof ( word );
     while ( cnt > 0)
     {
         *p = rand () & 0 xff ;
         p ++;
-        cnt - -;
+        cnt--;
     }
 }
 
@@ -153,15 +193,15 @@ Get Word Length/Bit Length/j-th Bit of BigInt
 void bignum_set_one ( bignum ** x)
  {
     bi_new (x , 1) ;
-    (* x) -> sign = NON_NEGATIVE ;
-    (* x) ->a [0] = 0 x1 ;
+    (*x) -> sign = NON_NEGATIVE ;
+    (*x) ->a[0] = 0 x1 ;
 }
 
  void bignum_set_zero ( bignum ** x )
 {
     bi_new (x , 1) ;
-    (* x) -> sign = NON_NEGATIVE ;
-    (* x) ->a [0] = 0 x0 ;
+    (*x) -> sign = NON_NEGATIVE ;
+    (*x) ->a[0] = 0 x0 ;
 }
 
 
@@ -170,10 +210,4 @@ void bignum_set_one ( bignum ** x)
 int main()
 {
     bignum a,b;
-    bignum_create(2, &a);
-    bignum_create(2, &b);
-    bignum_setuint(&a, 1234567890);
-    bignum_setuint(&b, 3692581470);
-    bignum_add(&a, &b);
-    
 }
