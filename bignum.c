188ef244bfa65074d66e3fffc25cee60679dbcb5
Yong Jin Lee <yjtarzan@kookmin.ac.kr>
2021-10-08 22:33:29 +0900

    main

--- bignum.c
+++ bignum.c
@@ -10,12 +10,11 @@ int main()
     word T[2] = { 0x0000cdef, 0x10234567 };
     // 0x 10234567 0000cdef
     bigint_set_by_array(&A, 0, T, 2);
+    // printf("%d", word);
     show_bigint_hex(A);
     bigint_delete(&A);
     return 0;
 }
-
-
 
 
 
