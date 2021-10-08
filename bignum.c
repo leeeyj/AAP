fd8da11a8ceadf44dff7a1e9bfbc0cd3b2d82eff
hikeoneasy <71643984+hikeoneasy@users.noreply.github.com>
2021-10-08 15:22:37 +0900

    Add files via upload

--- bignum.c
+++ bignum.c
@@ -1,155 +1,209 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+
+/*
+bignum에 우리가 원하는 크기 정해주면(ex unsigned char, unsigned int, unsigned longlong)
+64_size라는 자료형이 생성되고
+그걸 기반으로 사용할 연산들을 정의해주는 library구현입니다!!
+*/
+
+
+#include "bignum.h"
+
+
+int Dtype = 0; 
+
 
 
 /*
-ADD, SUB, MULTIPLE,  DIVISION, modulo,,,,암호에 쓰일 함수들...EC..???? 프린트함수  뭐 이런거 짜야해요~~~
-bignum에 우리가 원하는 크기 입력해주면(ex 64byte, 64_size)
-64_size라는 자료형이 생성되고
-그걸 기반으로 사용할 연산들을 정의해주는 library구현입니다!!
+    자료형의 크기를 선택할 수 있습니다.
 
+    이걸 Dtype 값을 0 1 -1 이런식으로 하고 싶은데 아이디어가 필요해요 위에처럼 global 변수로 선언하는게 맞을까요..?
 */
-typedef unsigned int u_int32;
+if(Dtype = 0)
+    typedef unsigned char word;
+else if(Dtype = 1)
+    typedef unsigned int word;
+else
+    typedef unsigned long long word;
+
 
 //freesize선언
-typedef struct freesize{
-    u_int32 len;
-    u_int32* dats;
+typedef struct {
+    int sign;           //NEGATIVE or NON - NEGATIVE
+    int wordlen;            // wordlen >= 0
+    word* a;          // address for big integer
 }bignum;
 
-//생성자
-u_int32 bignum_create(u_int32 len, bignum *xi)
+
+/*
+Zerorize 함수안에 arr init 구현하는건가요..???
+대충 느낌으로만 이렇게 구현함 ㅋㅋㅋ
+0 초기화 하는거 같아서,,,
+
+void array_init(word*a, int wordlen)
 {
-    xi->len=len;
-    // bignum 길이 
-    // u_int32 = 4-byte 
-    // 4byte 단위로 길이 
+    for(int i=0;i<worlen;i++)
+    {
+        a[i] =  0x0;
+    }
 
-    xi->dats=(u_int32*)malloc(len*sizeof(u_int32));
-    // 동적 메모리 할당
-    // len 만큼
-
-    if(xi->dats==NULL)
-        return 0;
-    
-    return -1;
 }
+*/    
 
-//u_int 형태의 data 넣기
-void bignum_setuint(bignum *dest, u_int32 src)
-{
-    dest->dats[0]=src;
-    memset(&dest->dats[1], 0, (dest->len-1)*sizeof(u_int32));
-}
-
-//bignum 형태의 데이터 넣기
-void bigint_set(bignum *dest, const bignum *src)
-{
-    memcpy(dest->dats,src->dats,
-        ((dest->len<src->len)?dest->len:src->len)*sizeof(u_int32));
-}
-
-//print 함수 필요
-
-//사이즈 변환..필요할까..?
-#if 0
-u_int bignum_resize(u_int newsize, bignum* xi)
-{
-    xi->len = newsize;
-    xi->dats = (u_int*)realloc(xi->dats, newsize * sizeof(u_int));
-    if (xi->dats == NULL)return 0;
-    return newsize;
-}
-#endif
 
 //제거자
-void bignum_release(bignum* xi)
+void bignum_delete(bignum* x)
 {
-    xi->len = 0;
-    free(xi->dats);
+    if (*x == NULL )
+        retrun ;
+#ifdef ZERORIZE                                 //얘 뭐임? ㅋㅋㅋㅋㅋㅋ
+    array_init((*x)->a,(*x)->wordlen);          
+#endif
+
+    free ((*x ) ->a );
+    free (*x ) ;
+    *x = NULL ;
 }
 
-
-//덧셈을 위한 준비01
-
-inline u_int32 add_a(u_int32 len,u_int32* dest,u_int32* src)
+void bignum_new ( bignum ** x , int wordlen )
 {
-    u_int32 carry=0;
-    for(u_int32 i=0;i<len;++i)
+    if (*x != NULL )
+    bignum_delete(x);
+
+    *x = ( bignum *) malloc ( sizeof ( bignum )) ;
+    (*x) -> sign = NON_NEGATIVE ; // 0
+    (*x) -> wordlen = wordlen ;
+    (*x) ->a = (word*) calloc ( wordlen , sizeof ( word ));
+}
+
+void bignum_refine ( bignum * x)
+{
+    if (x == NULL )
+    return ;
+
+    int new_wordlen = x -> wordlen;
+    while ( new_wordlen > 1) // at least one word needed
     {
-        dest[i]+=src[i];
-        if(carry)
-        {
-            if(dest[i]>=src[i])
-                carry=0;
-            if(dest[i]==0xFFFFFFFF)
-                carry=1;
-            ++dest[i];
-        }
-        else
-        {
-            if(dest[i]<src[i])
-                carry=1;
-        }
+        if (x -> a[ new_wordlen - 1] != 0)
+            break ;
+        new_wordlen--;
     }
-    return carry;
-}
-
-//덧셈을 위한 준비02
-
-inline u_int32 add_u4(u_int32 len,u_int32* dest,u_int32 src)
-{
-    u_int32 carry=0;
-    dest[0]+=src;
-    if(dest[0]<src)carry=1;
-    for(u_int32 i=1;i<len;++i)
+    if (x -> wordlen != new_wordlen )
     {
-        if(carry)
-        {
-            if(dest[i]!=0xFFFFFFFF)carry=0;
-            ++dest[i];
-        }
-        else
-        {
-            break;
-        }
+        x -> wordlen = new_wordlen ;
+        x ->a = ( word*) realloc (x - >a , sizeof( word )* new_wordlen );
     }
-    return carry;
+
+    if ((x - > wordlen == 1) && (x -> a [0] == 0x0 ))
+        x -> sign = NON_NEGATIVE ;
 }
 
-//bignum 덧셈
-void bignum_add(bignum *dest, const bignum *src)
+void bignum_assign ( bignum ** y , bignum *x)
 {
-    if(dest->len<src->len)
-    {
-        add_a(dest->len, dest->dats, src->dats);
-    }
-    else
-    {
-        if(add_a(src->len, dest->dats, src->dats))
-        {
-            add_u4(dest->len-src->len, &dest->dats[src->len], 1);
-        }
-    }
+    if (*y != NULL )
+    bignum_delete (y);
+    bignum_new (y , x -> wordlen ); 
+    (*y) -> sign = x -> sign ;
+    array_copy ((*y ) ->a , x ->a , x -> wordlen );
 }
 
-//u_int 덧셈
+/*
 
-void bignum_adduint(bignum *dest, u_int32 src,u_int32 pos)
+여기가 set함수 들어갈 자리
+void bi_set_by_array(bigint** x, int sign, word* a, int wordlen); // bigint x <- sign and array a
+void bi_set_by_string(bigint** x, int sign, char* str, int base); // bigint x <- base string
+void bi_show_hex(bigint* x); // show x in hexa. representation.
+void bi_show_dec(bigint* x); // show x in deci. representation.
+void bi_show_bin(bigint* x); // show x in binary representation
+
+
+
+*/  
+void bignum_set_by_array(bignum** x, int sign, word* a, int wordlen) // bigint x <- sign and array a
 {
-    if(dest->len<=pos)return;
-    add_u4(dest->len-pos,&dest->dats[pos],src);
+    if (*x != NULL )
+    bignum_delete (x);
+    bignum_new (x , wordlen );
+    (*x) -> sign = sign;
+    array_copy ((*x ) ->a , a , wordlen );
+
 }
+
+//맞게 구현한건가...?/???
+void bignum_set_by_string(bignum** x, int sign, char* str, int base)// bigint x <- base string        //////////////////base가 무슨 의미일까,,, 스트링 길이???
+{
+if (*x != NULL )
+    bignum_delete (x);
+    bignum_new (x , base );
+    (*x) -> sign = sign;
+    memcpy((*x ) ->a , str , base );
+}
+
+void bignum_show_hex(bignum* x)
+{
+    int len = (*x)->wordlen;
+    for(int i=0;i<len;i++)
+    {
+        printf("%x",(*x)->a[i]);
+    }
+    
+}
+
+void bignum_show_dec(bignum* x)
+{
+    // show얘네는 char int longlong나눠서 생각해야 하는거 아닌가요...?????
+    
+}
+
+void bignum_show_dec(bignum* x)
+{
+   
+    
+}
+
+
+void bignum_gen_rand ( bignum **x , int sign , int wordlen )
+{
+    bignum_new (x , wordlen );
+    (*x) -> sign = sign ;
+    array_rand ((*x) ->a , wordlen );
+    bignum_refine (*x);
+}
+
+void array_rand ( word* dst , int wordlen )
+{
+    byte*p = ( byte*) dst ;
+    int cnt = wordlen * sizeof ( word );
+    while ( cnt > 0)
+    {
+        *p = rand () & 0 xff ;
+        p ++;
+        cnt--;
+    }
+}
+
+/*
+Get Sign and Flip Sign of BitInt
+Get Word Length/Bit Length/j-th Bit of BigInt
+ Is Zero?, Is One?
+*/
+void bignum_set_one ( bignum ** x)
+ {
+    bi_new (x , 1) ;
+    (*x) -> sign = NON_NEGATIVE ;
+    (*x) ->a[0] = 0 x1 ;
+}
+
+void bignum_set_zero ( bignum ** x )
+{
+    bi_new (x , 1) ;
+    (*x) -> sign = NON_NEGATIVE ;
+    (*x) ->a[0] = 0 x0 ;
+}
+
+
 
 
 int main()
 {
     bignum a,b;
-    bignum_create(2, &a);
-    bignum_create(2, &b);
-    bignum_setuint(&a, 1234567890);
-    bignum_setuint(&b, 3692581470);
-    bignum_add(&a, &b);
-    
 }
