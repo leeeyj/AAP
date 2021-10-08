fa5df410983a513fd1754a9553f1aeb5b2201581
Yong Jin Lee <yjtarzan@kookmin.ac.kr>
2021-10-09 01:11:37 +0900

    add refine, gen_rand, etc

--- freeCize.h
+++ freeCize.h
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
 #define size_of_word 4
+#define NON_NEGATVE 0
+
+typedef char byte;
 
 #if size_of_word == 1
 typedef unsigned char word;
@@ -33,9 +39,23 @@ void bigint_set_by_string(bigint** x, int sign, char* str, int base);
 
 // print bigint
 void show_bigint_hex(bigint* x);
+void show_bigint_dec(bigint* x);
+void show_bigint_bin(bigint* x);
 
 // refine bigint, use realloc
 void bigint_refine(bigint* x);
 
 // generate random bigint
-void bigint_gen_rand();
+void bigint_gen_rand(bigint** x, int sign, int wordlen);
+void array_rand(word* dst, int wordlen);
+
+// Assign Bigint 
+void bigint_assign(bigint** y, bigint* x);
+
+// Set One, Set Zero
+void bigint_set_one(bigint** x);
+void bigint_set_zero(bigint** x);
+
+// IsZero? IsOne?
+bool IsZero(bigint* x);
+bool IsOne(bigint* x);
