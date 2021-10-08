7d0577e0d2de661d01211b635651fa2bd84d4169
Yong Jin Lee <yjtarzan@kookmin.ac.kr>
2021-10-09 01:10:36 +0900

    main

--- bignum.c
+++ bignum.c
@@ -7,10 +7,12 @@ int main()
 {
     bigint* A = NULL;
     // bigint_create(&A, 10);
-    word T[2] = { 0x0000cdef, 0x10234567 };
+    // word T[2] = { 0x0000cdef, 0x10234567 };
     // 0x 10234567 0000cdef
-    bigint_set_by_array(&A, 0, T, 2);
+    // bigint_set_by_array(&A, 0, T, 2);
     // printf("%d", word);
+    // show_bigint_hex(A);
+    bigint_gen_rand(&A, NON_NEGATVE, 2);
     show_bigint_hex(A);
     bigint_delete(&A);
     return 0;
