f8fa9044131f5f1781188a0aa487b0ae9dd6d6d8
Yong Jin Lee <yjtarzan@kookmin.ac.kr>
2021-10-10 14:57:22 +0900

    add Addition function

--- freeCize.h
+++ freeCize.h
@@ -1,8 +1,9 @@
 #include "./freeCize_core.h"
-#include <math.h>
+
 // Bigint Addition
-void ADD_ABC(word* x, word* y, int* c, word* C);             // Addition of Same signed Two Word
-bigint *ADDC(bigint* x, bigint* y);                // Addition of Same Signed Two Integers
-// void ADD();                 // Addition of Two Integers
+int ADD_ABC(word* x, word* y, int* c, word* C);             // Addition of Same signed Two Word
+bigint *ADDC(bigint* x, bigint* y);                         // Addition of Same Signed Two Integers
+// ADD();                                                   // Addition of Two Integers
 
 // Bigint Subtraction
+// ...
