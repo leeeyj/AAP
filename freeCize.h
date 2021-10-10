4b43ca18c6934cb3d54bcc2904c2e109f3408e55
Yong Jin Lee <yjtarzan@kookmin.ac.kr>
2021-10-10 12:37:27 +0900

    Addition, Subtaction, etc...

--- freeCize.h
+++ freeCize.h
@@ -1,68 +1,8 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
-#include <stdbool.h>
+#include "./freeCize_core.h"
+#include <math.h>
+// Bigint Addition
+void ADD_ABC(word* x, word* y, int* c, word* C);             // Addition of Same signed Two Word
+bigint *ADDC(bigint* x, bigint* y);                // Addition of Same Signed Two Integers
+// void ADD();                 // Addition of Two Integers
 
-#define size_of_word 4
-#define NON_NEGATVE 0
-#define NEGATIVE 1
-typedef char byte;
-
-#if size_of_word == 1
-typedef unsigned char word;
-#elif size_of_word == 4
-typedef unsigned int word;
-#elif size_of_word == 8;
-typedef unsigned long long word;
-#endif
-
-// typedef unsigned int word;
-// typedef unsigned char word;
-// typedef unsigned long long word;
-
-// BigInt struct
-typedef struct{
-    int sign;           // Negative or Positive
-    int wordlen;        // word length
-    word* a;            // address for big integer
-}bigint;
-
-// generate bigint
-void bigint_create(bigint** x, int wordlen);        // bigint dynamic memory allocation
-void bigint_delete(bigint** x);                     // bigint free memory 
-
-// bigint setting 
-void array_copy(word* A, word* T, int wordlen);                             // Copy word array
-void bigint_set_by_array(bigint** x, int sign, word* t, int wordlen);       // bigint setting by word array
-void bigint_set_by_string(bigint** x, int sign, char* str, int base);       // bigint setting by string
-
-// print bigint
-void show_bigint_hex(bigint* x);                // show bigint to hex
-void show_bigint_dec(bigint* x);                // show bigint to decimal
-void show_bigint_bin(bigint* x);                // show bigint to binary
-
-// refine bigint, use realloc
-void bigint_refine(bigint* x);                  // bigint memory reallocation 
-
-// generate random bigint
-void bigint_gen_rand(bigint** x, int sign, int wordlen);                // Generate Random bigint
-void array_rand(word* dst, int wordlen);                                // Generate Random word array
-// Only for Testing generate random bigint
-// void bigint_gen_rand1(bigint** x, int sign, int wordlen);
-// void array_rand1(word* dst, int wordlen);
-
-// Assign Bigint 
-void bigint_assign(bigint** y, bigint* x);          // Assign bigint ( y <- x )
-
-// Set One, Set Zero
-void bigint_set_one(bigint** x);                // ???
-void bigint_set_zero(bigint** x);               // ???
-
-// IsZero? IsOne?
-bool IsZero(bigint* x);                 // ???
-bool IsOne(bigint* x);                  // ???
-
-// Compare Two Bigint
-int CompareABS(bigint* x, bigint* y);           // Compare Absolute two integer 
-int Compare(bigint* x, bigint* y);              // Compare two integer
+// Bigint Subtraction
