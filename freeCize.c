1d4bbfad44c0dbcd42528253a0fc485178ae6558
Yong Jin Lee <yjtarzan@kookmin.ac.kr>
2021-10-10 14:57:24 +0900

    add Addition function

--- freeCize.c
+++ freeCize.c
@@ -1,15 +1,15 @@
 #include "freeCize.h"
 
 // ADDITION //
-void ADD_ABC(word* x, word* y, int* c, word* C)
-{
-    (*C) = ((*x) + (*y)) & (2*( 1<<(WordBitLen-1) - 1) + 1);             // (A + B) mod 2 ^ WordBitLen
-    if ((*C) < (*x)) *c = 1;
+int ADD_ABC(word* x, word* y, int* c, word* C)
+{   
+    (*C) = ((*x) + (*y));               // (A + B) mod 2 ^ WordBitLen
+    if ((*C) < (*x)) return *c = 1;    
 
     (*C) = ((*C) + (*c));
-    if ((*C) < (*c)) *c = 1;
+    if ((*C) < (*c)) return *c = 1;
 
-    (*c) = 0;
+    return *c = 0;
 }
 
 bigint *ADDC(bigint* x, bigint* y)
@@ -23,15 +23,19 @@ bigint *ADDC(bigint* x, bigint* y)
     bigint* sum = NULL;
     bigint_create(&sum, x->wordlen + 1);        // To save A + B, 최대 max(n, m) + 1 wordlen need
     int carry = 0;
-    for (int j = 0; j < x->wordlen; j++){
+
+
+    for (int j = 0; j < x->wordlen; j++){                       // Updating carry and C
         ADD_ABC(&(x->a[j]), &(y->a[j]), &carry, &C);
         sum->a[j] = C;
+        C = 0;
     }
     
-    if (carry == 1){
+    if (carry == 1){                    // If last carry is 1, sum = [1][word][word]...[word]
         sum->a[sum->wordlen - 1] = 1;
         return sum;
     }
-    bigint_refine(sum);
+
+    bigint_refine(sum);                 // reallocation bigint sum
     return sum;
 }
