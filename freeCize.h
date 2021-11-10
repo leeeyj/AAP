5dcccd228f0f53d0fd8a1cebd894bc8a0b426ff3
cryptogus <60291830+cryptogus@users.noreply.github.com>
2021-11-10 13:31:26 +0900

    뺄셈함수정의

---
+++ freeCize.h
@@ -0,0 +1,10 @@
+#include "./freeCize_core.h"
+
+// Bigint Addition
+int ADD_ABC(word* x, word* y, int* c, word* C);             // Addition of Same signed Two Word
+bigint* ADDC(bigint* x, bigint* y);                         // Addition of Same Signed Two Integers
+// ADD();                                                   // Addition of Two Integers
+
+// Bigint Subtraction
+int SUB_AbB(word* A, word* B, int* b, word* C);
+bigint* SUBC(bigint* A, bigint* B);
