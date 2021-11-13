935d8775a27844c0ef44b6bfab3f73f8a665f551
leeeyj <yjtarzan@kookmin.ac.kr>
2021-11-13 16:29:49 +0900

    Addition + Subtraction

--- freeCize.h
+++ freeCize.h
@@ -2,10 +2,10 @@
 
 // Bigint Addition
 int ADD_ABC(word* x, word* y, int* c, word* C);             // Addition of Same signed Two Word
-bigint *ADDC(bigint* x, bigint* y);                         // Addition of Same Signed Two Integers
+void ADDC(bigint* x, bigint* y, bigint** z);                         // Addition of Same Signed Two Integers
 // ADD();                                                   // Addition of Two Integers
 
 // Bigint Subtraction
 int SUB_AbB(word* A, word* B, int* b, word* C);
-bigint* SUBC(bigint* A, bigint* B);
+void SUBC(bigint* A, bigint* B, bigint** z);
 
