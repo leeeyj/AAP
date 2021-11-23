663f8e246ee8e4148252d21abe1169f59695431b
leeeyj <yjtarzan@kookmin.ac.kr>
2021-11-23 19:26:05 +0900

    Multiplication Testing

--- test.c
+++ test.c
@@ -17,7 +17,6 @@ unsigned char* array2string(bigint* x)
         if (s1 != NULL) memset(s1, 0, sizeof(word) * 2 + 1);
     }
     free(s1);
-
     return s2;
 }
 
@@ -26,21 +25,21 @@ int main()
     /*=========================================================================================================================================================*/
     // Setting Up Python for testing only
    
-    PyObject *pName, *pModule, *pFunc, *pValue, *pArg1, *pArg2, *pArg3;
+    PyObject *pName, *pModule, *pFunc, *pValue, *pArg1, *pArg2, *pArg3, *pArg4;
     Py_Initialize();                                                        // Python 인터프리터 초기화 
     if (Py_IsInitialized()) {                                               // Python 인터프리터 초기화 확인 
         pName = PyUnicode_FromString("test1");                              // Python File name -> test1.py, 초기화 필요! 
         pModule = PyImport_Import(pName);                                   // Import Python File            초기화 필요!
-        pFunc = PyObject_GetAttrString(pModule, "mul_k");                   // test1.py에 있는 add 함수 (= 함수 이름)  초기화 필요!
+        pFunc = PyObject_GetAttrString(pModule, "div");                   // test1.py에 있는 add 함수 (= 함수 이름)  초기화 필요!
         if (PyCallable_Check(pFunc)) {                                      // 함수가 잘 불러져 왔는지 확인
             int count = 0;
             printf("Testing : ");
             for (int i = 1; i < 100001; i++) {                               // 100000번 테스트
-                bigint* A = NULL;
-                bigint_gen_rand(&A, NON_NEGATVE, 17);
+                bigint* A = NULL; 
+                bigint_gen_rand(&A, NON_NEGATVE, 20);
                 
                 bigint* B = NULL;
-                bigint_gen_rand1(&B, NON_NEGATVE, 12);
+                bigint_gen_rand1(&B, NON_NEGATVE, 17);
                 
                 // bigint* sum = NULL;
                 // MULC_Naive(A, B, &sum);
@@ -48,8 +47,9 @@ int main()
                 // bigint* sub = NULL;
                 // SUBC(A, B, &sub);
                 
-                bigint* mul = NULL;
-                MUL(A, B, &mul);
+                bigint* Q = NULL;
+                bigint* R = NULL;
+                DIV(A, B, &Q, &R);
                 
                 // bigint* mul = NULL;
                 // MUL_AB(A, B, &mul);
@@ -60,15 +60,17 @@ int main()
                 unsigned char* string_B = array2string(B);
                 pArg2 = PyUnicode_FromString(string_B);
 
-                unsigned char* string_mul = array2string(mul);
-                pArg3 = PyUnicode_FromString(string_mul);
-
+                unsigned char* string_Q = array2string(Q);
+                pArg3 = PyUnicode_FromString(string_Q);
+                
+                unsigned char* string_R = array2string(R);
+                pArg4 = PyUnicode_FromString(string_R);
                 // Py_DECREF -> 메모리 누수 방지 
                 // printf("\nA : %s", string_A);
                 // printf("\nB : %s", string_B);
                 // printf("\nmul : %s", string_mul);
 
-                pValue = PyObject_CallFunctionObjArgs(pFunc, pArg1, pArg2, pArg3, NULL);     // 파이썬 mul_k 함수 실행 ( 매개 변수 여러 개 전달 ) 초기화 필요!
+                pValue = PyObject_CallFunctionObjArgs(pFunc, pArg1, pArg2, pArg3, pArg4, NULL);     // 파이썬 mul_k 함수 실행 ( 매개 변수 여러 개 전달 ) 초기화 필요!
                 // printf("%d", PyLong_AsLong(pValue));                                      // 인자가 더 이상 없다면 마지막에 NULL 
                 // printf("%d", PyLong_AsLong(pValue));
                 if (!PyLong_AsLong(pValue)) {
@@ -76,24 +78,30 @@ int main()
                     printf("\n");
                     printf("A : ");  show_bigint_hex(A);
                     printf("B : ");  show_bigint_hex(B);
-                    printf("MUL : ");  show_bigint_hex(mul);
+                    printf("Q : ");  show_bigint_hex(Q);
+                    printf("R : ");  show_bigint_hex(R);
 
                     Py_DECREF(string_A);
                     Py_DECREF(string_B);
-                    Py_DECREF(string_mul);
+                    Py_DECREF(string_Q);
+                    Py_DECREF(string_R);
+
 
                     Py_DECREF(pArg1);
                     Py_DECREF(pArg2);
                     Py_DECREF(pArg3);
+                    Py_DECREF(pArg4);
                     Py_DECREF(pValue);
 
                     bigint_delete(&A);
                     bigint_delete(&B);
-                    bigint_delete(&mul);
+                    bigint_delete(&Q);
+                    bigint_delete(&R);
 
                     free(string_A);
                     free(string_B);
-                    free(string_mul);
+                    free(string_Q);
+                    free(string_R);
 
                     Py_Finalize();
                     return 0;
@@ -105,16 +113,19 @@ int main()
                 Py_DECREF(pArg1);
                 Py_DECREF(pArg2);
                 Py_DECREF(pArg3);
+                Py_DECREF(pArg4);
                 Py_DECREF(pValue);
 
                 bigint_delete(&A);
                 bigint_delete(&B);
-                bigint_delete(&mul);
+                bigint_delete(&Q);
+                bigint_delete(&R);
 
                 free(string_A);
                 free(string_B);
-                free(string_mul);
-                
+                free(string_Q);
+                free(string_R);
+
             }
             printf(" : Test Success \n");
         }
