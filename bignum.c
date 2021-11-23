ea822ffaa99039357c820605966bb9739840150f
leeeyj <yjtarzan@kookmin.ac.kr>
2021-11-24 01:16:15 +0900

    bignum testing

--- bignum.c
+++ bignum.c
@@ -7,19 +7,29 @@ int main()
 {
     bigint* A = NULL;
     bigint* B = NULL;
+    bigint* N = NULL;
+    bigint* M = NULL;
 
     // Test generate random bigint 
-    bigint_gen_rand(&A, NON_NEGATVE, 11);
+    bigint_gen_rand(&A, NON_NEGATVE, 1);
+    bigint_gen_rand1(&N, NON_NEGATVE, 2);
     // bigint_create(&B, 10);
+
+    word T[1] = {0x12345678};
+    bigint_set_by_array(&M, NON_NEGATVE, T, 1);
 
     printf("A : ");
     show_bigint_hex(A);
     
+    Exponentiation(A, N, &B, M);
+    show_bigint_hex(B);
+    // show_bigint_hex(B);
     // printf("B : ");
     // show_bigint_hex(B);
     // printf("\n"); 
 
-    Sqr_karatsuba(A, &B);
+    // Test Squaring 
+    // Sqr_karatsuba(A, &B);
     // printf("A^2 = ");
     // show_bigint_hex(B);
 
@@ -39,6 +49,10 @@ int main()
     // Test deleting bigint
     bigint_delete(&A);
     bigint_delete(&B);
+    bigint_delete(&M);
+    bigint_delete(&N);
+
+
     // bigint_delete(&q);
     // bigint_delete(&r);
 
@@ -46,7 +60,6 @@ int main()
     // bigint_delete(&sub);
     //bigint_delete(&mul);
     // printf("%p", mul);
-    
 
 
     return 0;
