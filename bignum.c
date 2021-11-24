be919058b94bfe604efbed9730e9b9f40a5838c1
hikeoneasy <71643984+hikeoneasy@users.noreply.github.com>
2021-11-24 22:14:53 +0900

    Add files via upload

--- bignum.c
+++ bignum.c
@@ -5,6 +5,28 @@
 
 int main()
 {
+
+    /*
+    **********************************************************************************************************************************************************************
+    *                                                                                                                                                                    *
+    *                       The Enterstellar library was created for arithmetic on large integers.                                                                       *
+    *                                                                                                                                                                    *
+    *                       It was produced by Professor Dongchan Kim's class at Kookmin University.                                                                     *
+    *                                                                                                                                                                    *
+    *                       creator: Keon-hee Choi , Yong-jin Lee , Hyeon-ho Lee,  Yoon-seo Ki, Dong-chan Kim(not profeesor).                                             *
+    *                                                                                                                                                                    *
+    *                       production date:  21. 09. 01. - 21. 12. 08.                                                                                                  *
+    *                                                                                                                                                                    *
+    *                                                                                                                                                                    *
+    *                       Ownership of this code belongs to its creator(Keon-hee Choi , Yong-jin Lee , Hyun-ho Lee,  Yoon-seo Ki, Dong-chan Kim).                     *
+    *                                                                                                                                                                    *
+    *                       It is possible to reuse the code for free, but it is prohibited to make it a paid creation.                                                  *
+    *                                                                                                                                                                    *
+    *                       It can only be used through a contract with the creator.                                                                                     *
+    *                                                                                                                                                                    *
+    *                                                                                                                                                                    *
+    **********************************************************************************************************************************************************************
+    */
     
     return 0;
 }
