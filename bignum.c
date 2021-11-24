6f3908a45dfbc1506f02621be10aabc23898da65
Yong Jin Lee <yjtarzan@kookmin.ac.kr>
2021-11-24 21:33:13 +0900

    bignum

--- bignum.c
+++ bignum.c
@@ -5,16 +5,6 @@
 
 int main()
 {
-    bigint* A = NULL;
-    bigint* B = NULL;
-    bigint_gen_rand(&A, NON_NEGATVE, 1);
-    bigint_gen_rand1(&B, NON_NEGATVE, 1);
-
-    show_bigint_hex(A);
-    show_bigint_hex(B);
-    bigint* sum = NULL;
-    ADD(A, B, &sum);
-    show_bigint_hex(sum);
-
+    
     return 0;
 }
