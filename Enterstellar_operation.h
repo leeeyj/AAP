a55eff56ca6b4dff025e1962aa12ba78ab0fb737
leeeyj <yjtarzan@kookmin.ac.kr>
2021-11-27 02:41:02 +0900

    Enterstellar Operation

---
+++ Enterstellar_operation.h
@@ -0,0 +1,151 @@
+#include "./Enterstellar_core.h"
+/*************************************************************************************************************
+*																											 *
+*		Enterstellar_operation is used for operations on large integers.									 *
+*																											 *
+*		Component: Addition, Subtraction, Multiplication, Squaring, Division, Modular Exponentiation.	     *
+*																											 *
+**************************************************************************************************************/
+
+
+
+
+/*
+Bigint Addition : Single-Precision Addition & Multi-Precision Addition.
+
+[Single-Precision Addition]
+
+		ADD_ABC: Addition of Same Signed Word.
+			Input: word* x, word* y, int* carry.
+			Output: word*C.
+
+[Multi-Precision Addition]
+
+		ADDC: Addition of Same Signed Two Integers.
+			Input: bigint* x, bigint* y.
+			Output: bigint** z.
+
+		ADD:  Addition of Two Integers.
+			Input: bigint* x, bigint* y.
+			Output: bigint** z.
+
+*/
+// Bigint Addition
+void ADD_ABC(word* x, word* y, int* c, word* C);             // Addition of Same signed Two Word
+void ADDC(bigint* x, bigint* y, bigint** z);                 // Addition of Same Signed Two Integers
+void ADD(bigint* x, bigint* y, bigint** z);                  // Addition of Two Integers
+
+
+
+/*
+Bigint Subtraction: Single-Precision Subtraction & Multi-Precision Subtraction.
+
+[Single-Precision Subtraction]
+
+		SUB_AbB: Subtractio of Same signed word.
+			Input: word* A, word* B, int* borrow.
+			Output: word* C.
+
+[Multi-Precision Subtraction]
+
+		SUBC:  A-B of A >= B > 0.
+			Input: bigint* A, bigint* B.
+			output: bigint** z.
+
+		SUB: Subtraction of Two Integers.
+			Input: bigint* A, bigint* B.
+			output: bigint** z.
+
+*/
+// Bigint Subtraction
+void SUB_AbB(word* A, word* B, int* b, word* C);			 // Subtraction of Same Signed Word.
+void SUBC(bigint* A, bigint* B, bigint** z);				 // Subtraction of Same Signed Two Integers
+void SUB(bigint* x, bigint* y, bigint** z);					 // Subtraction of Two Integers.
+
+
+
+/*
+
+Bigint Multiplication: Single-Precision Multiplication & Multi-Precision Multiplication.
+[Single-Precision Multiplication]
+		MUL_AB: Multiplication of word.
+			Input: word* x, word* y.
+			Output: bigint** z.
+
+[Multi-Precision Multiplication]
+	We have two version, MULC_Naive and MULC_Karatsuba. We choose between MULC_Naive and MULC_Karatsuba via flag.
+	So, The mul function works using two functions.
+	mul -> Karatsuba ( If the flag is not satisfied, you go to Naive version)
+
+
+		MULC_Naive: Multiplication Naive Version
+			Input: bigint* x, bigint* y
+			Output: bigint** z
+
+
+		MULC_Karatsuba: Fast Multiplication Algorithm
+			Input: bigint* x, bigint* y
+			Output: bigint** z
+
+*/
+// Bigint Multiplication 
+void MUL_AB(word* x, word* y, bigint** z);                  // Single word Multiplication
+void MULC_Naive(bigint* x, bigint* y, bigint** z);          // Multiplication Naive Version 
+void MULC_Karatsuba(bigint* x, bigint* y, bigint** z);      // Fast Multiplication Algorithm
+void MUL(bigint* x, bigint* y, bigint** z);
+
+
+
+
+// Bigint Division
+void Long_Division(bigint* A, bigint* B, bigint** Q);
+void DIVCC(bigint* A, bigint* B, bigint** Q, bigint** R);
+void DIVC(bigint* A, bigint* B, bigint** Q, bigint** R);
+void DIV(bigint* A, bigint* B, bigint** Q, bigint** R);
+
+
+
+/*
+
+Bigint Squaring : Single-Precision Squaring & Multi-Precision Squaring.
+
+[Single-Precision Squaring]
+	Single_percision_sqr
+		Input: word A.
+		Output: bigint** C
+
+[Multi-Precision Squaring]
+	We have three versions. [ SQU, textbook, Karatsuba]
+	When the flag is not satisfied, it provides fast squaring processing for inputs 0, 1, -1 and processing for the remaining values.
+	If the flag is satisfied, high-speed operation is performed using Karatsuba Squaring.
+
+	SQU: Processing for 0,1,-1
+		Input: bigint* x
+		Output: bigint** y
+
+
+	Sqr_Textbook: Squring Naive Version
+		Input: bigint* x
+		Output: bigint** y
+
+	Sqr_karatsuba: Fast Squring  Version
+		Input: bigint* x
+		Output: bigint** y
+
+*/
+// Bigint Squaring 
+void Single_percision_sqr(word A, bigint** C);          //Word Squaring
+void Sqr_Textbook(bigint* x, bigint** y);				//Squring Naive Version
+void Sqr_karatsuba(bigint* x, bigint** y);				//Fast Squring  Version
+void SQU(bigint* x, bigint** y);						//Processing for 0,1,-1
+
+
+
+
+
+//bigint modulo exponential
+void Exponentiation(bigint* x, bigint* N, bigint** z, bigint* M);
+// void Exponentiation2(bigint* x, int N, bigint** z);
+//void Modular_Exponentiation(bigint* x, int N, bigint** z);
+//void Montgomery_reduction(bigint* x, bigint* N,bigint**z);
+
