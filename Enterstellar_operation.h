37189135b37b8bc1bcddae4403b5ee67d0861c29
leeeyj <yjtarzan@kookmin.ac.kr>
2021-12-12 15:41:52 +0900

    Enterstellar_operation

--- Enterstellar_operation.h
+++ Enterstellar_operation.h
@@ -31,7 +31,7 @@ Bigint Addition : Single-Precision Addition & Multi-Precision Addition.
 
 */
 // Bigint Addition
-void ADD_ABC(word* x, word* y, int* c, word* C);             // Addition of Same signed Two Word
+void ADD_ABC(word* x, word* y, unsigned int* c, word* C);             // Addition of Same signed Two Word
 void ADDC(bigint* x, bigint* y, bigint** z);                 // Addition of Same Signed Two Integers
 void ADD(bigint* x, bigint* y, bigint** z);                  // Addition of Two Integers
 
@@ -58,7 +58,7 @@ Bigint Subtraction: Single-Precision Subtraction & Multi-Precision Subtraction.
 
 */
 // Bigint Subtraction
-void SUB_AbB(word* A, word* B, int* b, word* C);			 // Subtraction of Same Signed Word.
+void SUB_AbB(word* A, word* B, unsigned int* b, word* C);			 // Subtraction of Same Signed Word.
 void SUBC(bigint* A, bigint* B, bigint** z);				 // Subtraction of Same Signed Two Integers
 void SUB(bigint* x, bigint* y, bigint** z);					 // Subtraction of Two Integers.
 
