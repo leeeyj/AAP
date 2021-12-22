a7235d0bf79956353853421bdf7dff97c222aaa6
leeeyj <yjtarzan@kookmin.ac.kr>
2021-12-23 03:23:35 +0900

    Enterstellar Core

--- Enterstellar_core.h
+++ Enterstellar_core.h
@@ -20,10 +20,10 @@
 
 */
 
-#define WordBitLen 64
-#define NON_NEGATVE 0
+#define WordBitLen 32
+#define NON_NEGATIVE 0
 #define NEGATIVE 1
-typedef char byte;
+// typedef char byte;
 
 #if WordBitLen == 8
 typedef unsigned char word;
@@ -51,11 +51,10 @@ void bigint_delete(bigint** x);                     // bigint free memory
 // bigint setting 
 void array_copy(word* A, word* T, int wordlen);                             // Copy word array
 void bigint_set_by_array(bigint** x, int sign, word* t, int wordlen);       // bigint setting by word array
-void bigint_set_by_string(bigint** x, int sign, char* str, int base);       // bigint setting by string
+
 
 // print bigint
 void show_bigint_hex(bigint* x);                // show bigint to hex
-void show_bigint_dec(bigint* x);                // show bigint to decimal
 void show_bigint_bin(bigint* x);                // show bigint to binary
 
 // refine bigint, use realloc
@@ -69,12 +68,12 @@ void array_rand(word* dst, int wordlen);                                // Gener
 void bigint_assign(bigint** y, bigint* x);          // Assign bigint ( y <- x )
 
 // Set One, Set Zero
-void bigint_set_one(bigint** x);                // ???
-void bigint_set_zero(bigint** x);               // ???
+void bigint_set_one(bigint** x);                
+void bigint_set_zero(bigint** x);              
 
 // IsZero? IsOne?
-bool IsZero(bigint* x);                 // ???
-bool IsOne(bigint* x);                  // ???
+bool IsZero(bigint* x);                
+bool IsOne(bigint* x);                  
 
 // Compare Two Bigint
 int CompareABS(bigint* x, bigint* y);           // Compare Absolute two integer 
